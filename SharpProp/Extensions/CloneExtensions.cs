f7622b1a7be65f8f5c8cc683396c3cea7f652f5d
Vladimir Portyanikhin <86243191+portyanikhin@users.noreply.github.com>
2022-09-02 14:33:37 +0300

    Refactoring

--- SharpProp/Extensions/CloneExtensions.cs
+++ SharpProp/Extensions/CloneExtensions.cs
@@ -8,8 +8,7 @@
         /// <param name="instance">The fluid instance.</param>
         /// <returns>Deep copy of the fluid instance.</returns>
         public static Fluid Clone(this Fluid instance) =>
-            instance.Factory()
-                .WithState(instance.Inputs[0], instance.Inputs[1]);
+            instance.WithState(instance.Inputs[0], instance.Inputs[1]);
 
         /// <summary>
         ///     Performs deep (full) copy of the mixture instance.
@@ -17,8 +16,7 @@
         /// <param name="instance">The mixture instance.</param>
         /// <returns>Deep copy of the mixture instance.</returns>
         public static Mixture Clone(this Mixture instance) =>
-            instance.Factory()
-                .WithState(instance.Inputs[0], instance.Inputs[1]);
+            instance.WithState(instance.Inputs[0], instance.Inputs[1]);
 
         /// <summary>
         ///     Performs deep (full) copy of the humid air instance.
@@ -26,7 +24,6 @@
         /// <param name="instance">The humid air instance.</param>
         /// <returns>Deep copy of the humid air instance.</returns>
         public static HumidAir Clone(this HumidAir instance) =>
-            instance.Factory()
-                .WithState(instance.Inputs[0], instance.Inputs[1], instance.Inputs[2]);
+            instance.WithState(instance.Inputs[0], instance.Inputs[1], instance.Inputs[2]);
     }
 }

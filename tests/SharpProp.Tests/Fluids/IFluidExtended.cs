07771235c33864b15309a612135d9de6cbf1bb16
Vladimir Portyanikhin <86243191+portyanikhin@users.noreply.github.com>
2023-12-27 11:59:51 +0300

    Updated formatting

--- tests/SharpProp.Tests/Fluids/IFluidExtended.cs
+++ tests/SharpProp.Tests/Fluids/IFluidExtended.cs
@@ -9,86 +9,86 @@ public interface IFluidExtended : IFluid
     /// <summary>
     ///     Mass specific constant volume specific heat (by default, kJ/kg/K).
     /// </summary>
-    public SpecificEntropy SpecificHeatConstVolume { get; }
+    SpecificEntropy SpecificHeatConstVolume { get; }
 
     /// <summary>
     ///     Molar density (by default, kg/mol).
     /// </summary>
-    public MolarMass? MolarDensity { get; }
+    MolarMass? MolarDensity { get; }
 
     /// <summary>
     ///     Ozone depletion potential (ODP).
     /// </summary>
-    public double? OzoneDepletionPotential { get; }
+    double? OzoneDepletionPotential { get; }
 
     /// <inheritdoc cref="IFluid.WithState"/>
-    public new IFluidExtended WithState(
+    new IFluidExtended WithState(
         IKeyedInput<Parameters> firstInput,
         IKeyedInput<Parameters> secondInput
     );
 
     /// <inheritdoc cref="IFluid.IsentropicCompressionTo"/>
-    public new IFluidExtended IsentropicCompressionTo(Pressure pressure);
+    new IFluidExtended IsentropicCompressionTo(Pressure pressure);
 
     /// <inheritdoc cref="IFluid.CompressionTo"/>
-    public new IFluidExtended CompressionTo(
+    new IFluidExtended CompressionTo(
         Pressure pressure,
         Ratio isentropicEfficiency
     );
 
     /// <inheritdoc cref="IFluid.IsenthalpicExpansionTo"/>
-    public new IFluidExtended IsenthalpicExpansionTo(Pressure pressure);
+    new IFluidExtended IsenthalpicExpansionTo(Pressure pressure);
 
     /// <inheritdoc cref="IFluid.IsentropicExpansionTo"/>
-    public new IFluidExtended IsentropicExpansionTo(Pressure pressure);
+    new IFluidExtended IsentropicExpansionTo(Pressure pressure);
 
     /// <inheritdoc cref="IFluid.IsentropicExpansionTo"/>
-    public new IFluidExtended ExpansionTo(
+    new IFluidExtended ExpansionTo(
         Pressure pressure,
         Ratio isentropicEfficiency
     );
 
     /// <inheritdoc cref="IFluid.CoolingTo(Temperature, Pressure?)"/>
-    public new IFluidExtended CoolingTo(
+    new IFluidExtended CoolingTo(
         Temperature temperature,
         Pressure? pressureDrop = null
     );
 
     /// <inheritdoc cref="IFluid.CoolingTo(SpecificEnergy, Pressure?)"/>
-    public new IFluidExtended CoolingTo(
+    new IFluidExtended CoolingTo(
         SpecificEnergy enthalpy,
         Pressure? pressureDrop = null
     );
 
     /// <inheritdoc cref="IFluid.HeatingTo(Temperature, Pressure?)"/>
-    public new IFluidExtended HeatingTo(
+    new IFluidExtended HeatingTo(
         Temperature temperature,
         Pressure? pressureDrop = null
     );
 
     /// <inheritdoc cref="IFluid.HeatingTo(SpecificEnergy, Pressure?)"/>
-    public new IFluidExtended HeatingTo(
+    new IFluidExtended HeatingTo(
         SpecificEnergy enthalpy,
         Pressure? pressureDrop = null
     );
 
     /// <inheritdoc cref="IFluid.BubblePointAt(Pressure)"/>
-    public new IFluidExtended BubblePointAt(Pressure pressure);
+    new IFluidExtended BubblePointAt(Pressure pressure);
 
     /// <inheritdoc cref="IFluid.BubblePointAt(Temperature)"/>
-    public new IFluidExtended BubblePointAt(Temperature temperature);
+    new IFluidExtended BubblePointAt(Temperature temperature);
 
     /// <inheritdoc cref="IFluid.DewPointAt(Pressure)"/>
-    public new IFluidExtended DewPointAt(Pressure pressure);
+    new IFluidExtended DewPointAt(Pressure pressure);
 
     /// <inheritdoc cref="IFluid.DewPointAt(Temperature)"/>
-    public new IFluidExtended DewPointAt(Temperature temperature);
+    new IFluidExtended DewPointAt(Temperature temperature);
 
     /// <inheritdoc cref="IFluid.TwoPhasePointAt"/>
-    public new IFluidExtended TwoPhasePointAt(Pressure pressure, Ratio quality);
+    new IFluidExtended TwoPhasePointAt(Pressure pressure, Ratio quality);
 
     /// <inheritdoc cref="IFluid.Mixing"/>
-    public IFluidExtended Mixing(
+    IFluidExtended Mixing(
         Ratio firstSpecificMassFlow,
         IFluidExtended first,
         Ratio secondSpecificMassFlow,
@@ -96,8 +96,8 @@ public interface IFluidExtended : IFluid
     );
 
     /// <inheritdoc cref="IClonable{T}.Clone"/>
-    public new IFluidExtended Clone();
+    new IFluidExtended Clone();
 
     /// <inheritdoc cref="IFactory{T}.Factory"/>
-    public new IFluidExtended Factory();
+    new IFluidExtended Factory();
 }
--- tests/SharpProp.Tests/HumidAir/IHumidAirExtended.cs
+++ tests/SharpProp.Tests/HumidAir/IHumidAirExtended.cs
@@ -10,85 +10,85 @@ public interface IHumidAirExtended : IHumidAir
     ///     Mass specific constant volume
     ///     specific heat per humid air (by default, kJ/kg/K).
     /// </summary>
-    public SpecificEntropy SpecificHeatConstVolume { get; }
+    SpecificEntropy SpecificHeatConstVolume { get; }
 
     /// <inheritdoc cref="IHumidAir.WithState"/>
-    public new IHumidAirExtended WithState(
+    new IHumidAirExtended WithState(
         IKeyedInput<string> firstInput,
         IKeyedInput<string> secondInput,
         IKeyedInput<string> thirdInput
     );
 
     /// <inheritdoc cref="IHumidAir.DryCoolingTo(Temperature, Pressure?)"/>
-    public new IHumidAirExtended DryCoolingTo(
+    new IHumidAirExtended DryCoolingTo(
         Temperature temperature,
         Pressure? pressureDrop = null
     );
 
     /// <inheritdoc cref="IHumidAir.DryCoolingTo(SpecificEnergy, Pressure?)"/>
-    public new IHumidAirExtended DryCoolingTo(
+    new IHumidAirExtended DryCoolingTo(
         SpecificEnergy enthalpy,
         Pressure? pressureDrop = null
     );
 
     /// <inheritdoc cref="IHumidAir.WetCoolingTo(Temperature, RelativeHumidity, Pressure?)"/>
-    public new IHumidAirExtended WetCoolingTo(
+    new IHumidAirExtended WetCoolingTo(
         Temperature temperature,
         RelativeHumidity relativeHumidity,
         Pressure? pressureDrop = null
     );
 
     /// <inheritdoc cref="IHumidAir.WetCoolingTo(Temperature, Ratio, Pressure?)"/>
-    public new IHumidAirExtended WetCoolingTo(
+    new IHumidAirExtended WetCoolingTo(
         Temperature temperature,
         Ratio humidity,
         Pressure? pressureDrop = null
     );
 
     /// <inheritdoc cref="IHumidAir.WetCoolingTo(SpecificEnergy, RelativeHumidity, Pressure?)"/>
-    public new IHumidAirExtended WetCoolingTo(
+    new IHumidAirExtended WetCoolingTo(
         SpecificEnergy enthalpy,
         RelativeHumidity relativeHumidity,
         Pressure? pressureDrop = null
     );
 
     /// <inheritdoc cref="IHumidAir.WetCoolingTo(SpecificEnergy, Ratio, Pressure?)"/>
-    public new IHumidAirExtended WetCoolingTo(
+    new IHumidAirExtended WetCoolingTo(
         SpecificEnergy enthalpy,
         Ratio humidity,
         Pressure? pressureDrop = null
     );
 
     /// <inheritdoc cref="IHumidAir.HeatingTo(Temperature, Pressure?)"/>
-    public new IHumidAirExtended HeatingTo(
+    new IHumidAirExtended HeatingTo(
         Temperature temperature,
         Pressure? pressureDrop = null
     );
 
     /// <inheritdoc cref="IHumidAir.HeatingTo(SpecificEnergy, Pressure?)"/>
-    public new IHumidAirExtended HeatingTo(
+    new IHumidAirExtended HeatingTo(
         SpecificEnergy enthalpy,
         Pressure? pressureDrop = null
     );
 
     /// <inheritdoc cref="IHumidAir.HumidificationByWaterTo(RelativeHumidity)"/>
-    public new IHumidAirExtended HumidificationByWaterTo(
+    new IHumidAirExtended HumidificationByWaterTo(
         RelativeHumidity relativeHumidity
     );
 
     /// <inheritdoc cref="IHumidAir.HumidificationByWaterTo(Ratio)"/>
-    public new IHumidAirExtended HumidificationByWaterTo(Ratio humidity);
+    new IHumidAirExtended HumidificationByWaterTo(Ratio humidity);
 
     /// <inheritdoc cref="IHumidAir.HumidificationBySteamTo(RelativeHumidity)"/>
-    public new IHumidAirExtended HumidificationBySteamTo(
+    new IHumidAirExtended HumidificationBySteamTo(
         RelativeHumidity relativeHumidity
     );
 
     /// <inheritdoc cref="IHumidAir.HumidificationBySteamTo(Ratio)"/>
-    public new IHumidAirExtended HumidificationBySteamTo(Ratio humidity);
+    new IHumidAirExtended HumidificationBySteamTo(Ratio humidity);
 
     /// <inheritdoc cref="IHumidAir.Mixing"/>
-    public IHumidAirExtended Mixing(
+    IHumidAirExtended Mixing(
         Ratio firstSpecificMassFlow,
         IHumidAirExtended first,
         Ratio secondSpecificMassFlow,
@@ -96,8 +96,8 @@ public interface IHumidAirExtended : IHumidAir
     );
 
     /// <inheritdoc cref="IClonable{T}.Clone"/>
-    public new IHumidAirExtended Clone();
+    new IHumidAirExtended Clone();
 
     /// <inheritdoc cref="IFactory{T}.Factory"/>
-    public new IHumidAirExtended Factory();
+    new IHumidAirExtended Factory();
 }

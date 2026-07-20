04b22e2185fea323a49191679cac0352414c35fc
Vladimir Portyanikhin <86243191+portyanikhin@users.noreply.github.com>
2026-07-21 00:55:27 +0300

    refactor: inline extension classes into related source files

--- src/SharpProp/IJsonable.cs
+++ src/SharpProp/IJsonable.cs
@@ -12,3 +12,17 @@ public interface IJsonable
     /// <returns>A JSON string.</returns>
     string AsJson(bool indented = true);
 }
+
+internal static class JsonExtensions
+{
+    private static readonly JsonSerializerSettings Settings = new()
+    {
+        Converters = [new StringEnumConverter(), new UnitsNetIQuantityJsonConverter()],
+    };
+
+    public static string ConvertToJson(this object? instance, bool indented)
+    {
+        Settings.Formatting = indented ? Formatting.Indented : Formatting.None;
+        return JsonConvert.SerializeObject(instance, Settings);
+    }
+}

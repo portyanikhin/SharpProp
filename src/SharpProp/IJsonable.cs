d1ad429cabc55921121ecadd7bb55af944f6ec01
Vladimir Portyanikhin <86243191+portyanikhin@users.noreply.github.com>
2026-07-21 16:13:15 +0300

    fix(json): prevent race condition in serialization settings

--- src/SharpProp/IJsonable.cs
+++ src/SharpProp/IJsonable.cs
@@ -15,14 +15,16 @@ public interface IJsonable
 
 internal static class JsonExtensions
 {
-    private static readonly JsonSerializerSettings Settings = new()
-    {
-        Converters = [new StringEnumConverter(), new UnitsNetIQuantityJsonConverter()],
-    };
+    public static string ConvertToJson(this object? instance, bool indented) =>
+        JsonConvert.SerializeObject(
+            instance,
+            CreateSettings(indented ? Formatting.Indented : Formatting.None)
+        );
 
-    public static string ConvertToJson(this object? instance, bool indented)
-    {
-        Settings.Formatting = indented ? Formatting.Indented : Formatting.None;
-        return JsonConvert.SerializeObject(instance, Settings);
-    }
+    private static JsonSerializerSettings CreateSettings(Formatting formatting) =>
+        new()
+        {
+            Converters = [new StringEnumConverter(), new UnitsNetIQuantityJsonConverter()],
+            Formatting = formatting,
+        };
 }
